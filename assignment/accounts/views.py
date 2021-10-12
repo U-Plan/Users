8ce9219cdcdf67899b2510aba6888f9f9c0c3336
Jeongil-Eum <barl3131@gmail.com>
2021-10-12 16:40:57 +0900

    add signup

--- assignment/accounts/views.py
+++ assignment/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .serializers import UserCreateSerializer, UserSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserCreate(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({'id': user.id}, status=status.HTTP_201_CREATED)
