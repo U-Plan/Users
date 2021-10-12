12279d6166d90691def03f915543b950148e3d88
Jeongil-Eum <barl3131@gmail.com>
2021-10-12 20:56:05 +0900

    fix remove urls slash

--- assignment/accounts/urls.py
+++ assignment/accounts/urls.py
@@ -3,9 +3,9 @@ from . import views
 from rest_framework import urls
 
 urlpatterns = [
-    path('auth/', views.SmsAuth.as_view()),
+    path('auth', views.SmsAuth.as_view()),
     path('auth/certificate', views.SmsCertificate.as_view()),
-    path('signup/', views.UserCreate.as_view()),
-    path('login/', views.UserLogin.as_view()),
-    path('info/', views.UserInfo.as_view()),
+    path('signup', views.UserCreate.as_view()),
+    path('login', views.UserLogin.as_view()),
+    path('info', views.UserInfo.as_view()),
 ]
